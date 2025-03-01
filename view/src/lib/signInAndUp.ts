e69a9e02a672a9f26ca3b3b65ecfb58881c9bdf2
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-01 13:09:24 +0900

    ログイン処理の実装とendpointを整形する関数を実装

--- view/src/lib/signInAndUp.ts
+++ view/src/lib/signInAndUp.ts
@@ -1,5 +1,6 @@
 import type { DBUser, User } from "@/types";
 import type { User as FirebaseUser } from "@firebase/auth";
+import { get_url } from "@/lib/utils";
 
 const storeStorageUser = (user: DBUser) => {
 	localStorage.setItem("userID", JSON.stringify(user));
@@ -9,7 +10,18 @@ export const signInOrUp = async (firebaseUser: FirebaseUser) => {
 	// TODO: サインインまたはサインアップ処理を実装する
 	console.log("サインインまたはサインアップが完了しました")
 
-	toRoot();
+	// TODO: バックエンドが完成したら繋ぎこむ
+	// const res = await fetch(get_url("/v1/users"), {
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// 	body: JSON.stringify({
+	// 		google_id: firebaseUser.uid,
+	// 	}),
+	// });
+
+	// toRoot();
 };
 
 const signUp = async (user: User) => {
--- view/src/lib/utils.ts
+++ view/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function get_url(path: string) {
+  return `${process.env.NEXT_PUBLIC_API_URL}${path}`
+}
