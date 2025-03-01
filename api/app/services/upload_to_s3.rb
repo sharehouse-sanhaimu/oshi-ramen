c15cb9eb3bd4ab36cc8bc06edc9e91f8f2637eec
dishizawa <dishizawa2001@gmail.com>
2025-03-02 01:43:38 +0900

    blobではなく、元の拡張子のままS3に保存されるように修正

--- api/app/services/upload_to_s3.rb
+++ api/app/services/upload_to_s3.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk-s3'
+require 'mime/types'
 
 class UploadToS3
   def self.call(file)
@@ -9,10 +10,18 @@ class UploadToS3
     )
 
     key = "uploads/#{SecureRandom.uuid}_#{file.original_filename}"
+
+    content_type = (
+      file.content_type ||
+      MIME::Types.type_for(file.original_filename).first&.content_type ||
+      'application/octet-stream'
+    )
+
     s3.put_object(
       bucket: ENV['AWS_BUCKET_NAME'],
       key: key,
       body: file,
+      content_type: content_type
     )
 
     "https://#{ENV['AWS_BUCKET_NAME']}.s3.#{ENV['AWS_REGION']}.amazonaws.com/#{key}"
