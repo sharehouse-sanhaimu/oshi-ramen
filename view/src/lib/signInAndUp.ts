42d8a84ceb4cd4581521f4176baf63d54cc206c1
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-01 17:19:20 +0900

    update signInOrUp

--- view/src/lib/signInAndUp.ts
+++ view/src/lib/signInAndUp.ts
@@ -1,49 +1,45 @@
-import type { User as FirebaseUser } from "@firebase/auth";
 import { get_url } from "@/lib/utils";
+import type { User as FirebaseUser } from "@firebase/auth";
 
 const storeStorageUser = (uid: string) => {
 	localStorage.setItem("userID", uid);
 };
 
-export const signInOrUp = async (firebaseUser: FirebaseUser) => {
-	// TODO: サインインまたはサインアップ処理を実装する
-	storeStorageUser(firebaseUser.uid);
+type responseJson = {
+	message: string;
+	data: {
+		id: string;
+	};
+};
 
+export const signInOrUp = async (firebaseUser: FirebaseUser) => {
+	console.log("firebaseUser uid", firebaseUser.uid);
+	console.log("firebaseUser displayName", firebaseUser.displayName);
 	try {
-		// TODO: バックエンドが完成したら繋ぎこむ
-		// const res = await fetch(get_url("/v1/users"), {
-		// 	method: "POST",
-		// 	headers: {
-		// 		"Content-Type": "application/json",
-		// 	},
-		// 	body: JSON.stringify({
-		// 		google_id: firebaseUser.uid,
-		// 	}),
-		// });
-		const responseMock = {
-			status: 200,
-			data: {
-				id: "1",
+		const res = await fetch("http://localhost:8000/v1/users", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
 			},
-		}
-		const res = responseMock;
+			body: JSON.stringify({
+				google_id: firebaseUser.uid,
+				nickname: firebaseUser.displayName || "default_nickname", // ニックネームを追加
+			}),
+		});
 
 		if (res.status === 200) {
-			console.log("ユーザーが存在します")
-		}
-		else if (res.status === 201) {
+			console.log("ユーザーが存在します");
+		} else if (res.status === 201) {
 			console.log("ユーザーを作成しました");
-		}
-		else if (res.status === 422) {
+		} else if (res.status === 422) {
 			console.log("リクエストが不正です", res);
 			return;
-		}
-		else {
+		} else {
 			console.log("予期せぬエラーが発生しました", res);
 			return;
 		}
 
-		const responseJson = res;
+		const responseJson = (await res.json()) as responseJson;
 		const uid = responseJson.data.id;
 		storeStorageUser(uid);
 
@@ -52,12 +48,6 @@ export const signInOrUp = async (firebaseUser: FirebaseUser) => {
 	} catch (error) {
 		console.error("エラーが発生しました:", error);
 	}
-
-};
-
-const signUp = async () => {
-	// TODO: 
-	console.log("ユーザー登録が完了しました")
 };
 
 const toRoot = () => {
