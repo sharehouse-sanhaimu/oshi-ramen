b5a47852959b183da10d9c85d0818c9d44688c18
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-01 12:47:43 +0900

    fix google login

--- view/src/lib/signInAndUp.ts
+++ view/src/lib/signInAndUp.ts
@@ -6,75 +6,13 @@ const storeStorageUser = (user: DBUser) => {
 };
 
 export const signInOrUp = async (firebaseUser: FirebaseUser) => {
-	try {
-		const res = await fetch(`/api/user/?uid=${firebaseUser.uid}`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
-
-		if (res.status === 200) {
-			const userData = await res.json();
-			const user: DBUser = { ...userData };
-
-			if (userData) {
-				storeStorageUser(user);
-				if (!userData.experiencePoint) {
-					await createExp(userData.id);
-				}
-				toRoot();
-			}
-		} else if (res.status === 404) {
-			await signUp(firebaseUser);
-			// サインアップ後にユーザー情報を取得してログイン処理を行う
-			await signInOrUp(firebaseUser);
-		} else {
-			console.error(`Unexpected status code: ${res.status}`);
-		}
-	} catch (error) {
-		console.error("エラーが発生しました:", error);
-	}
+	// TODO: サインインまたはサインアップ処理を実装する
+	console.log("サインインまたはサインアップが完了しました")
 };
 
 const signUp = async (user: User) => {
-	try {
-		const res = await fetch("/api/user/new", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(user),
-		});
-
-		if (res.status === 200) {
-			// ユーザー登録が成功した場合は何もせず戻る
-			return;
-		}
-		throw new Error("ユーザー登録に失敗");
-	} catch (error) {
-		console.error("エラーが発生しました:", error);
-	}
-};
-
-const createExp = async (userId: number) => {
-	try {
-		const res = await fetch("/api/experiencePoint/new", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(userId),
-		});
-
-		if (res.status === 200) {
-			toRoot();
-		} else {
-			throw new Error("作成に失敗しました");
-		}
-	} catch (error) {
-		console.error("エラーが発生しました:", error);
-	}
+	// TODO: 
+	console.log("ユーザー登録が完了しました")
 };
 
 const toRoot = () => {
