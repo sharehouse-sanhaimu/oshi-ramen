dd03f7d5d06ac489743270248e10baa31f302728
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-01 21:50:15 +0900

    fast api環境構築

---
+++ magazine/main.py
@@ -0,0 +1,34 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from routes import test
+
+app = FastAPI()
+
+EXECUTING_ENVIRONMENT = os.getenv("EXECUTING_ENVIRONMENT")
+print(f"EXECUTING_ENVIRONMENT: {EXECUTING_ENVIRONMENT}")
+
+origins = [
+    os.getenv("ACSESS_ALLOW_URL"),  # Next.jsアプリケーションのオリジン
+    # 必要に応じて他のオリジンも追加
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 許可するオリジンのリスト
+    allow_credentials=True,
+    allow_methods=["*"],  # すべてのメソッドを許可
+    allow_headers=["*"],  # すべてのヘッダーを許可
+)
+
+
+if EXECUTING_ENVIRONMENT == "prod":
+    pass
+
+elif EXECUTING_ENVIRONMENT == "dev":
+    app.include_router(test.router)
+
+else:
+    pass
