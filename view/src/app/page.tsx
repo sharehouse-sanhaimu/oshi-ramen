f3e4e401373bf45c59ce1a851824a2d829d14f9a
nose221834 <iatyy01151821@gmail.com>
2025-03-01 15:45:15 +0900

    fix: ファイルの有無でFormの表示inputを切り替え

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -22,22 +22,23 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 
 export default function Home() {
+	const [userId, setUserId] = useState<string | null>(null);
+	const [isFile, setIsFile] = useState<boolean>(false);
+
 	const form = useForm<Post>({
 		resolver: zodResolver(postSchema),
 		defaultValues: {
 			user_id: "",
 			store_name: "",
-			file: undefined,
 			ramen_name: "",
+			file: undefined,
 			delicious: 3,
 			portion: 3,
 			thick: 3,
 			texture: 3,
-			soup: 3, // 例えば range の場合は 0 など適切な値
+			soup: 3,
 		},
 	});
-
-	const [userId, setUserId] = useState<string | null>(null);
 
 	useEffect(() => {
 		const auth = getAuth();
@@ -61,16 +62,16 @@ export default function Home() {
 		console.log(data);
 	};
 
-	const fileValue = form.watch("file");
-
-	console.log(form.watch());
+	const fileDelete = () => {
+		setIsFile(false);
+	};
 
 	return (
-		<>
-			<div className="flex flex-col min-h-screen px-10 py-10 bg-gradient-to-t from-gray-300 via-gray-200 to-gray-50">
-				<div className="flex flex-col items-center justify-center space-y-6">
-					<Form {...form}>
-						<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+		<div className="flex flex-col min-h-screen px-10 py-10 bg-gradient-to-t from-gray-300 via-gray-200 to-gray-50">
+			<div className="flex flex-col items-center justify-center space-y-6">
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+						<div className={`${isFile ? "hidden" : ""}`}>
 							<FormField
 								control={form.control}
 								name="file"
@@ -80,152 +81,173 @@ export default function Home() {
 										<FormControl>
 											<Input
 												type="file"
-												onChange={(e) => field.onChange(e.target.files)}
+												accept="image/*"
+												onChange={(e) => {
+													const files = e.target.files;
+													field.onChange(files);
+													setIsFile(true);
+												}}
 											/>
 										</FormControl>
 										<FormMessage />
 									</FormItem>
 								)}
 							/>
+						</div>
 
-							<div>
-								{fileValue ? (
-									<div>ファイルがアップロードされました</div>
-								) : (
-									<div>まだファイルがアップロードされていません</div>
-								)}
-							</div>
+						{isFile ? (
+							<>
+								<FormField
+									control={form.control}
+									name="store_name"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Store Name</FormLabel>
+											<FormControl>
+												<Input placeholder="お店の名前" {...field} />
+											</FormControl>
+											{/* <FormDescription>
+								This is your public display name.
+							</FormDescription> */}
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="ramen_name"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Ramen Name</FormLabel>
+											<FormControl>
+												<Input placeholder="ラーメンの名前" {...field} />
+											</FormControl>
+											{/* <FormDescription>
+								This is your public display name.
+							</FormDescription> */}
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="delicious"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Delicious</FormLabel>
+											<FormControl>
+												<Input
+													type="range"
+													min={1}
+													max={5}
+													placeholder="おいしさ"
+													{...field}
+												/>
+											</FormControl>
+											{/* <FormDescription>
+								This is your public display name.
+							</FormDescription> */}
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="portion"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Portion</FormLabel>
+											<FormControl>
+												<Input
+													type="range"
+													min={1}
+													max={5}
+													placeholder="量"
+													{...field}
+												/>
+											</FormControl>
+											{/* <FormDescription>
+								This is your public display name.
+							</FormDescription> */}
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="thick"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Thick</FormLabel>
+											<FormControl>
+												<Input
+													type="range"
+													min={1}
+													max={5}
+													placeholder="太さ"
+													{...field}
+												/>
+											</FormControl>
+											{/* <FormDescription>
+								This is your public display name.
+							</FormDescription> */}
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="texture"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Texture</FormLabel>
+											<FormControl>
+												<Input
+													type="range"
+													min={1}
+													max={5}
+													placeholder="コシ"
+													{...field}
+												/>
+											</FormControl>
+											{/* <FormDescription>
+								This is your public display name.
+							</FormDescription> */}
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								<FormField
+									control={form.control}
+									name="soup"
+									render={({ field }) => (
+										<FormItem>
+											<FormLabel>Soup</FormLabel>
+											<FormControl>
+												<Input
+													type="range"
+													min={1}
+													max={5}
+													placeholder="コッテリ"
+													{...field}
+												/>
+											</FormControl>
+											{/* <FormDescription>
+								This is your public display name.
+							</FormDescription> */}
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</>
+						) : (
+							<></>
+						)}
 
-							<FormField
-								control={form.control}
-								name="store_name"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Store Name</FormLabel>
-										<FormControl>
-											<Input placeholder="お店の名前" {...field} />
-										</FormControl>
-										{/* <FormDescription>
-								This is your public display name.
-							</FormDescription> */}
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="ramen_name"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Ramen Name</FormLabel>
-										<FormControl>
-											<Input placeholder="ラーメンの名前" {...field} />
-										</FormControl>
-										{/* <FormDescription>
-								This is your public display name.
-							</FormDescription> */}
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="delicious"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Delicious</FormLabel>
-										<FormControl>
-											<Input
-												type="range"
-												min={1}
-												max={5}
-												placeholder="おいしさ"
-												{...field}
-											/>
-										</FormControl>
-										{/* <FormDescription>
-								This is your public display name.
-							</FormDescription> */}
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="portion"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Portion</FormLabel>
-										<FormControl>
-											<Input type="range" min={1} max={5} placeholder="量" {...field} />
-										</FormControl>
-										{/* <FormDescription>
-								This is your public display name.
-							</FormDescription> */}
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="thick"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Thick</FormLabel>
-										<FormControl>
-											<Input type="range" min={1} max={5} placeholder="太さ" {...field} />
-										</FormControl>
-										{/* <FormDescription>
-								This is your public display name.
-							</FormDescription> */}
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="texture"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Texture</FormLabel>
-										<FormControl>
-											<Input type="range" min={1} max={5} placeholder="コシ" {...field} />
-										</FormControl>
-										{/* <FormDescription>
-								This is your public display name.
-							</FormDescription> */}
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="soup"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Soup</FormLabel>
-										<FormControl>
-											<Input
-												type="range"
-												min={1}
-												max={5}
-												placeholder="コッテリ"
-												{...field}
-											/>
-										</FormControl>
-										{/* <FormDescription>
-								This is your public display name.
-							</FormDescription> */}
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-
-							<Button type="submit">Submit</Button>
-						</form>
-					</Form>
-				</div>
+						<Button type="submit">Submit</Button>
+					</form>
+				</Form>
 			</div>
-		</>
+		</div>
 	);
 }
