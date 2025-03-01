9b378bd7190c429c5adfde1ac8904d16fdb5b1c6
nose221834 <iatyy01151821@gmail.com>
2025-03-01 12:27:32 +0900

    add: formの雛形を作成

--- view/src/app/post/page.tsx
+++ view/src/app/post/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { postSchema } from "@/types/post";
+import type { Post } from "@/types/post";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { type SubmitHandler, useForm } from "react-hook-form";
 
 type Inputs = {
@@ -11,26 +14,36 @@ export default function App() {
 	const {
 		register,
 		handleSubmit,
-		watch,
 		formState: { errors },
-	} = useForm<Inputs>();
-	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
-
-	console.log(watch("example")); // watch input value by passing the name of it
+	} = useForm<Post>({
+		resolver: zodResolver(postSchema),
+	});
+	const onSubmit = (data: Post) => {
+		console.log(data);
+	};
 
 	return (
 		/* "handleSubmit" will validate your inputs before invoking "onSubmit" */
 		<form onSubmit={handleSubmit(onSubmit)}>
-			{/* register your input into the hook by invoking the "register" function */}
-			<input defaultValue="test" {...register("example")} />
+			<input {...register("store_name")} />
 
-			{/* include validation with required or other standard HTML validation rules */}
-			<input {...register("exampleRequired", { required: true })} />
-			{/* errors will return when field validation fails  */}
-			{errors.exampleRequired && <span>This field is required</span>}
+			<input {...register("ramen_name")} />
+
+			<input {...register("parameter.delicious")} />
+
+			<input {...register("parameter.portion")} />
+
+			<input {...register("parameter.thick")} />
+
+			<input {...register("parameter.texture")} />
+
+			<input {...register("parameter.soup")} />
+
+			<input {...register("file.file")} />
+
+			{errors.file && <span>This field is required</span>}
 
 			<input type="submit" />
 		</form>
 	);
 }
-
