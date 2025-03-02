b3a0c4baf2b05931feedad07403ef6b3d458d33c
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-02 12:28:28 +0900

    update: print route

--- magazine/routes/print_magazine.py
+++ magazine/routes/print_magazine.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+
 from fastapi import APIRouter
 from modules.print_magazine import print_magazine
 
 router = APIRouter()
 
+BASE_DIR = Path("out")
+
 
 @router.post("/print_magazine")
-def post_magazine(magazine_path: str):
+def post_magazine(user_id: int):
+    magazine_path = BASE_DIR / str(user_id) / "magazine.pdf"
     print_magazine(
         magazine_path,  # "./out/output_page_1.jpg"
     )
