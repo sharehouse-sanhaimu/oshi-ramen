4ce9977a3cb0743b0add79cbcef1ad602568ec50
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-02 11:45:30 +0900

    add: fetch ramen info

--- magazine/modules/fetch_ramen_info.py
+++ magazine/modules/fetch_ramen_info.py
@@ -1,20 +1,29 @@
 import os
+import random
 
 import requests
 
 
-def fetch_ramen_info(id: str):
+def fetch_ramen_info(id: str) -> list:
     api_endpoint = os.getenv("API_ENDPOINT")
     url = f"{api_endpoint}/v1/ramen?user_id={id}"
     print("url:", url)
 
-    response = requests.get(
-        url,
-        headers={
-            "Content-Type": "application/json"
-            }
-        )
+    response = requests.get(url, headers={"Content-Type": "application/json"})
     print(response.status_code)
+    print(response.json(), len(response.json()))
+
+    extracted_data = []
+    id_set = set()
+    for _ in range(3):
+        _id = random.randint(0, len(response.json()) - 1)
+        if _id in id_set:
+            continue
+        id_set.add(_id)
+        extracted_data.append(response.json()[_id])
+    print(extracted_data)
+    
+    return extracted_data
 
 
 if __name__ == "__main__":
