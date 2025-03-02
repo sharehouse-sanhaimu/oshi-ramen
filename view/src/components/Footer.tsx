7d731edc97748716d2b95359500577c8ebdfafd5
nose221834 <iatyy01151821@gmail.com>
2025-03-02 15:30:22 +0900

    fix: color変更

--- view/src/components/Footer.tsx
+++ view/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
 	return (
-		<footer className=" bg-yellow-600 text-white p-4 mt-8">
+		<footer className=" bg-emerald-500 text-white p-4 mt-8">
 			<div className="container mx-auto text-center">
 				<p>© 2025 ShareHouse. All rights reserved.</p>
 			</div>
--- view/src/components/Header.tsx
+++ view/src/components/Header.tsx
@@ -3,11 +3,29 @@ import React from "react";
 
 export const Header = () => {
 	return (
-		<header className="bg-yellow-600 text-white p-4">
-			<div className="container mx-auto">
+		<header className="bg-emerald-500  shadow-lg py-4">
+			<div className="container mx-auto flex items-center justify-between px-4">
 				<Link href="/">
-					<h1 className="text-xl font-bold italic cursor-pointer">推し麺ノート</h1>
+					<h1 className=" text-amber-50 text-2xl font-extrabold italic tracking-wide cursor-pointer hover:text-yellow-200 transition-colors duration-300">
+						推し麺ノート
+					</h1>
 				</Link>
+				<nav>
+					<ul className="flex space-x-6">
+						<li>
+							<Link href="/" className="hover:text-yellow-200 transition-colors duration-300">
+							</Link>
+						</li>
+						<li>
+							<Link href="/about" className="hover:text-yellow-200 transition-colors duration-300">
+							</Link>
+						</li>
+						<li>
+							<Link href="/contact" className="hover:text-yellow-200 transition-colors duration-300">
+							</Link>
+						</li>
+					</ul>
+				</nav>
 			</div>
 		</header>
 	);
