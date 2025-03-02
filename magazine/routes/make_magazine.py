ebc6c3641f757cd066aa8d4d1c9e53347e778026
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-02 12:49:23 +0900

    fix: magazine handler

--- magazine/routes/make_magazine.py
+++ magazine/routes/make_magazine.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from handler.magazine_handler import make_magazine
+from handler.magazine_handler import magazine_handler
 
 router = APIRouter()
 
 
 @router.post("/make_magazine")
 def generate_magazine(user_id: int):
-    make_magazine(user_id)
+    magazine_handler(user_id)
     return {"message": "Success"}
