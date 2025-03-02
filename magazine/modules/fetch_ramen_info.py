e8893611b0eaa5d897b39def370bb8fc10b01bd4
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-02 11:23:12 +0900

    add: fetch module

---
+++ magazine/modules/fetch_ramen_info.py
@@ -0,0 +1,16 @@
+import os
+
+import requests
+
+
+def fetch_ramen_info(id: str):
+    api_endpoint = os.getenv("API_ENDPOINT")
+    url = f"{api_endpoint}/v1/ramen?user_id={id}"
+    print("url:", url)
+
+    response = requests.get(url)
+    print(response.json())
+
+
+if __name__ == "__main__":
+    fetch_ramen_info("1")
