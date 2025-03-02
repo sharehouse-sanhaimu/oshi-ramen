8270fdb9529543b45eeeff4db2c23b97bd8663db
AobaIwaki123 <aobaiwaki@gmail.com>
2025-03-02 11:38:38 +0900

    add: clean

--- magazine/modules/fetch_ramen_info.py
+++ magazine/modules/fetch_ramen_info.py
@@ -8,9 +8,14 @@ def fetch_ramen_info(id: str):
     url = f"{api_endpoint}/v1/ramen?user_id={id}"
     print("url:", url)
 
-    response = requests.get(url)
-    print(response.json())
+    response = requests.get(
+        url,
+        headers={
+            "Content-Type": "application/json"
+            }
+        )
+    print(response.status_code)
 
 
 if __name__ == "__main__":
-    fetch_ramen_info("1")
+    fetch_ramen_info("2")
