57907d38c2f31a88e8695116f80ad6a6cf7b9fd6
nose221834 <iatyy01151821@gmail.com>
2025-03-02 10:46:00 +0900

    fix: 画像一覧を修正

--- view/src/components/Ramen.tsx
+++ view/src/components/Ramen.tsx
@@ -2,37 +2,36 @@
 
 import { RamenModal } from "@/components/RamenModal";
 import type { RamenGallery } from "@/types/RamenGallery";
-import Image from "next/image";
 import { useState } from "react";
 
 type RamenGallerysProps = {
-	imageInfo: RamenGallery;
+  imageInfo: RamenGallery;
 };
 
 export const Ramen = ({ imageInfo }: RamenGallerysProps) => {
-	const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-	const handleOpenModal = () => setIsOpenModal(true);
+  const handleOpenModal = () => setIsOpenModal(true);
 
-	return (
-		<>
-			<button
-				type="button"
-				onClick={handleOpenModal}
-				className="relative w-full pb-[100%] overflow-hidden cursor-pointer border-0 p-0 group"
-			>
-				<img
-					src={imageInfo.image_url}
-					alt={imageInfo.name}
-					className="object-cover"
-				/>
-				<div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-30 transition duration-300" />
-			</button>
-			<RamenModal
-				{...imageInfo}
-				isOpenModal={isOpenModal}
-				setIsOpenModal={setIsOpenModal}
-			/>
-		</>
-	);
+  return (
+    <>
+      <button
+        type="button"
+        onClick={handleOpenModal}
+        className="relative w-full pb-[100%] overflow-hidden cursor-pointer border-0 p-0 group"
+      >
+        <img
+          src={imageInfo.image_url}
+          alt={imageInfo.name}
+          className="absolute inset-0 w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-white opacity-0 group-hover:opacity-30 transition duration-300" />
+      </button>
+      <RamenModal
+        {...imageInfo}
+        isOpenModal={isOpenModal}
+        setIsOpenModal={setIsOpenModal}
+      />
+    </>
+  );
 };
